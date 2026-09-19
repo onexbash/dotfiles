799e8b893925dd4dcf5ac023ae5b2f911ad10f17
onexbash <fabian@schlegel.one>
2026-09-19 15:25:35 +0200

    fix: Update PATH constructing logic to use global scope and check for directory existance with null glob qualifier

--- shell/config/path.zsh
+++ shell/config/path.zsh
@@ -5,7 +5,7 @@
 # -- Path Handler -- #
 function construct_path() {
   # Deduplicate entries and tie scalar $PATH to array $path
-  typeset -U path PATH
+  typeset -gU path PATH
   
   # Add paths from /etc/paths & /etc/paths.d/*
   local system_paths=()
@@ -13,12 +13,11 @@ function construct_path() {
   if [[ -f "/etc/paths" ]]; then
     system_paths+=(${(f)"$(< /etc/paths)"})
   fi 
-
-  if [[ -d "/etc/paths.d" ]]; then
-    for file in /etc/paths.d/*(N); do
-      [[ -f "$file" ]] && system_paths+=(${(f)"$(< "$file")"})
-    done
-  fi 
+ 
+  local file
+  for file in /etc/paths.d/*(N.); do
+     system_paths+=(${(f)"$(< "$file")"})
+  done
 
   # Default Path Entries
   local default_paths=(  
@@ -30,19 +29,20 @@ function construct_path() {
   )
 
   # Custom Path Entries
+  # note: '(N-/)' drops the entry if no directory exists for a path entry
   local custom_paths=(
     # Homebrew
-    "/opt/homebrew/bin"
-    "/opt/homebrew/sbin"
+    /opt/homebrew/bin(N-/)
+    /opt/homebrew/sbin(N-/)
     # GNU Utils
-    "/opt/homebrew/opt/coreutils/libexec/gnubin"
-    "/opt/homebrew/opt/grep/libexec/gnubin"
+    /opt/homebrew/opt/coreutils/libexec/gnubin(N-/)
+    /opt/homebrew/opt/grep/libexec/gnubin(N-/)
     # Rust / Cargo
-    "${CARGO_HOME:-$XDG_CONFIG_HOME/cargo}/bin"
+    ${CARGO_HOME:-$XDG_CONFIG_HOME/cargo}/bin(N-/)
     # ASDF
-    "${ASDF_DATA_DIR:-$HOME/.asdf}/shims"
+    ${ASDF_DATA_DIR:-$HOME/.asdf}/shims(N-/)
     # GoLang
-    "${GOPATH:-$HOME/go}/bin"
+    ${GOPATH:-$HOME/go}/bin(N-/)
   )
   
   # Clear & construct PATH (high-to-low priority)
